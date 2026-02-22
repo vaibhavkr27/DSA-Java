581e1fc7cb6f45a684fd0816e9b1e414d8a6c87f
Vaibhav Kumar <vk27112006@gmail.com>
2026-02-23 01:33:58 +0530

    Move zero leetcode(easy)

--- src/Arrays/TwoPointers/MoveZeros.java
+++ src/Arrays/TwoPointers/MoveZeros.java
@@ -1,4 +1,21 @@
 package Arrays.TwoPointers;
 
 public class MoveZeros {
+    class Solution {
+        public void moveZeroes(int[] nums) {
+            int l=0;
+            int r=0;
+            while(r<nums.length){
+                if(nums[r]!=0){
+                    int temp=nums[l];
+                    nums[l]=nums[r];
+                    nums[r]=temp;
+                    l++;
+
+                }
+                r++;
+            }
+
+        }
+    }
 }
